19fbc85040ccf78617dbea311e1692f094dd34df
Pablo Orellana <pablooren18@gmail.com>
2021-02-03 23:35:23 -0600

    recibir hola mundo desde un pequeño servidor

--- tarea_edd.go
+++ tarea_edd.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"fmt" // manejo de informacion
+	"log"
+	"net/http" // manejo de servidores
+
+	//"log" // manejo de errores en servidores
+	"github.com/gorilla/mux" //usar api de manera sencilla
 )
 
+func GetDatos(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola mundo Juan Pablo Orellana 201314881")
+
+}
+
 func main() {
-	nombre := "Juan Pablo Orellana"
-	carnet := 201314881
-	fmt.Printf("Hola mundo %v %v", nombre, carnet)
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", GetDatos)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
 
 }
